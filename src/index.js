e1f31703753cefa38bd2737a4fb158d435abb1ea
Suraj <surajmitake2003@gmail.com>
2024-12-29 22:22:19 +0530

    express app configure

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import connectDB from "./db/index.js";
+import app from "./app.js"
 
-connectDB();
+const PORT = process.env.PORT || 8000;
+
+connectDB()
+.then(() => {
+    app.listen(PORT, () => {
+        console.log("Server is Running on port ", PORT);
+    })
+})
+.catch((err) => {
+    console.log("DB Connection Failed !!! ", err);
+})
